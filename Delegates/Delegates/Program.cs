051b06c2014ab067958f8c4448303be8380fc92a
Safal08 <Safal Mahat>
2021-07-25 23:16:38 +0545

    Partial class and delegate added

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Delegates
+{    
+    
+    
+    public class Program
+    {
+        int? a = null; //Value type lai nullable banauna ? use garxam
+
+        string aa = null;
+        //int -> type safe vayo tara int matra rakhna pryo
+        //object -> type safe vako xaina tara hamle j rakhe ni hunxa
+        //Generic -> type safe + jun value rakhe ni paune
+        public bool Compare<T>(T a, T b)
+        {
+            if (a.Equals(b))
+                return true;
+            return false;
+            
+        }
+       
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            bool result = p.Compare<string>("Hello","Hello");
+
+            Console.WriteLine("The sum is {0}", result);
+
+        }
+    }
+}
---
+++ Delegates/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b = new Button();
+            b.Text = "Click ME!!";
+            b.Size = new Size(100, 50);
+            b.Location = new Point(100, 100);
+            this.Controls.Add(b);
+        }
+    }
+}
